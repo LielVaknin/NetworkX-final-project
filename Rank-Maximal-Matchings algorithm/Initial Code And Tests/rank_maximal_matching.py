7a9242ce9ff9fb6f765bc9142d2fbd7d2ed71c02
Oriya Alperin <73301483+oriyalperin@users.noreply.github.com>
2021-12-30 21:40:49 +0200

    Update tests & signature

--- Rank-Maximal-Matchings algorithm/Initial Code And Tests/rank_maximal_matching.py
+++ Rank-Maximal-Matchings algorithm/Initial Code And Tests/rank_maximal_matching.py
@@ -1,35 +1,25 @@
 """
 Returns the rank maximal matching of the ranked bipartite graph `G`.
-
     A ranked graph is a graph in which every edge has a rank [1,r] such that 1
     is the highest rank, and then 2 is the next highest rank, and so on.
-
     A matching is a set of edges that do not share any nodes.
-
     A rank-maximal matching is one with the maximum
     possible number of edges with the first rank, and subject to that condition,
     the maximum possible number of edges with the second rank, and so on.
-
-
     Parameters
     ----------
     G : NetworkX graph
-
       Undirected weighted (the weight of every edge represents the rank) bipartite graph
-
     Returns
     -------
     M : dictionary
        The matching is returned as a dictionary, `matching`, such that
          ``matching[v] == w`` if node `v` is matched to node `w`. Unmatched
         nodes do not occur as a key in `matching`.
-
     Examples
     --------
     In the biaprtite graph, G = (V,E). with the sets V1 as 0 and V2 as 1,
     and the weight of the edges as the ranks.
-
-
         >>> G = nx.DiGraph()
         >>> G.add_nodes_from(['a1', 'a2'], bipartite=0)
         >>> G.add_nodes_from(['p1', 'p2'], bipartite=1)
@@ -39,7 +29,6 @@ Returns the rank maximal matching of the ranked bipartite graph `G`.
         {'a1': 'p2', 'p2': 'a1'}
         >>>m['a1']
         'p2'
-
         explanation:                            2
                         G =             a1-----------p1
                                          \
@@ -52,13 +41,10 @@ Returns the rank maximal matching of the ranked bipartite graph `G`.
                                                 \
                                            2     \
                                     a2-----------p2
-
          The matching M1 is {'a1':'p2', 'p2':'a1'} so O1, EV1 and U1 are  {a1,p2},{a2,p1},{} respectively.
          After removing the edges incident to O1 with the rank higher than 1 {(a1,p1),(a2,p2)} there are no more edges
          to add to G1, so an augmenting path doesnt exists and the algorithm ends returning M1.
-
         -------
-
         >>> G = nx.DiGraph()
         >>> G.add_nodes_from(['a1', 'a2', 'a3'], bipartite=0)
         >>> G.add_nodes_from(['p1', 'p2'], bipartite=1)
@@ -68,12 +54,6 @@ Returns the rank maximal matching of the ranked bipartite graph `G`.
         {'a1': 'p1', 'a2': 'p2', 'p1': 'a1', 'p2': 'a2'}
         >>> m['a1']
         'p1'
-
-
-
-
-
-
     Raises
     ------
     AmbiguousSolution
@@ -81,31 +61,26 @@ Returns the rank maximal matching of the ranked bipartite graph `G`.
       with all nodes in one bipartite set is provided. When determining
       the nodes in each bipartite set more than one valid solution is
       possible if the input graph is disconnected.
-
     Notes
     -----
     This function uses the algorithm published in the article of Irving et al. (2006), "Rank maximal matching".
-
     See :mod:`bipartite documentation <networkx.algorithms.bipartite>`
     for further details on how bipartite graphs are handled in NetworkX.
-
     See Also
     --------
     maximum_matching
     hopcroft_karp_matching
-
     References
     ----------
     Irving, Robert W. and Kavitha, Telikepalli and Mehlhorn, Kurt and Michail, Dimitrios and Paluch, Katarzyna E.,
     "Rank-Maximal Matchings",ACM Trans. Algorithms,2006,Association for Computing Machinery**
        https://citeseerx.ist.psu.edu/viewdoc/download?doi=10.1.1.92.6742&rep=rep1&type=pdf
-
     """
 
 
-def rank_maximal_matching(G):
+def rank_maximal_matching(G, rank="rank"):
     M = {}
-    
+
     return M
 
 
@@ -119,8 +94,6 @@ return- EVi - set of even vretices
 
 """def divide_to_sets(Gi):
     return EVi, Oi, Ui
-
-
 def divide_vertices_by_rank(G):
     return {}
 """
@@ -133,12 +106,8 @@ return - list_of_free_vertices
 
 """def find_free_vertices(Gi):
     return list_of_free_vertices
-
-
 def create_Gi_plus_1(Gi, rank):
     return Gi
-
-
 def get_G1(G):
     return G1"""
 
@@ -157,8 +126,5 @@ remove OiOi edges
 
 """def remove_edges(G, Oi, Ui, rank_i):
     return
-
-
 def get_reverse_of_augmenting_path(Gi):
     return augmenting_path"""
-
--- Rank-Maximal-Matchings algorithm/Initial Code And Tests/tests.py
+++ Rank-Maximal-Matchings algorithm/Initial Code And Tests/tests.py
@@ -4,21 +4,104 @@ import rank_maximal_matching as rmm
 
 
 class rank_maximal_matching(unittest.TestCase):
+    def test_rank_maximal_matching_bigger_left(self):
+        G=nx.Graph()
+        matching = {'a1': 'p1', 'a2': 'p2', 'p1': 'a1', 'p2': 'a2'}
+        G.add_nodes_from(['a1', 'a2', 'a3'], bipartite=0)
+        G.add_nodes_from(['p1', 'p2'], bipartite=1)
+        G.add_weighted_edges_from([('a1', 'p1', 1), ('a1', 'p2', 2), ('a2', 'p2', 1), ('a3', 'p2', 1)])
+        # our function
+        M = rmm.rank_maximal_matching(G)
+        self.assertEqual(M, matching)
+
+    def test_rank_maximal_matching_bigger_right(self):
+        matching = {'a1': 'p2', 'a3': 'p4', 'a2': 'p1', 'p2': 'a1', 'p4': 'a3', 'p1': 'a2'}
+        G = nx.Graph()
+        # Add nodes with the node attribute 'bipartite'
+        G.add_nodes_from(['a1', 'a2', 'a3'], bipartite=0)
+        G.add_nodes_from(['p1', 'p2', 'p3', 'p4'], bipartite=1)
+        # Add edges only between nodes of opposite node sets
+        G.add_weighted_edges_from([('a1', 'p1', 1), ('a1', 'p2', 1), ('a2', 'p2', 1), ('a2', 'p3', 2), ('a3', 'p4', 1)])
+        # our function
+        M = rmm.rank_maximal_matching(G, rank="weight")
+        self.assertEqual(M, matching)
+
+    def test_rank_maximal_matching_same_size(self):
+        matching = {'a1': 'p2', 'a3': 'p1', 'p2': 'a1','p1': 'a3'}
+        G = nx.Graph()
+        # Add nodes with the node attribute 'bipartite'
+        G.add_nodes_from(['a1', 'a2', 'a3'], bipartite=0)
+        G.add_nodes_from(['p1', 'p2', 'p3'], bipartite=1)
+        # Add edges only between nodes of opposite node sets
+        G.add_weighted_edges_from([('a1', 'p2', 1), ('a1', 'p3', 2), ('a2', 'p2', 2), ('a3', 'p1', 1), ('a3', 'p2', 2)])
+        # our function
+        M = rmm.rank_maximal_matching(G,rank="weight")
+        self.assertEqual(M, matching)
+
+    def test_rank_maximal_matching_disconnected_graph(self):
+        matching = {'a1': 'p2', 'a2': 'p1', 'a3': 'p5', 'a4': 'p3',
+                    'p2': 'a1', 'p1': 'a2', 'p5': 'a3', 'p3': 'a4'}
+        G = nx.Graph()
+        G.add_nodes_from(['a1', 'a2', 'a3','a4','a5'], bipartite=0)
+        G.add_nodes_from(['p1', 'p2', 'p3','p4','p5'], bipartite=1)
+        G.add_weighted_edges_from([('a1', 'p1', 1), ('a1', 'p2', 1), ('a1', 'p5', 2),
+                                   ('a2', 'p1', 1), ('a2', 'p2', 2),('a2', 'p3', 2),
+                                   ('a3', 'p2', 1),('a3', 'p4', 2),('a3', 'p5', 1),
+                                   ('a4', 'p3', 2),('a4', 'p4', 3),('a4', 'p5', 2)])
+
+
+        M = rmm.rank_maximal_matching(G, rank="weight")
+        self.assertEqual(M, matching)
+
+
+    # should throw an exception
+    # ignore
+    def test_rank_maximal_matching_negative_rank(self):
+        G = nx.Graph()
+
+    def test_rank_maximal_matching_perfect_matching(self):
+        matching = {'a1': 'p2', 'a3': 'p1', 'a2': 'p3', 'p2': 'a1', 'p1': 'a3', 'p3': 'a2'}
+        G = nx.Graph()
+        # Add nodes with the node attribute 'bipartite'
+        G.add_nodes_from(['a1', 'a2', 'a3'], bipartite=0)
+        G.add_nodes_from(['p1', 'p2', 'p3'], bipartite=1)
+        # Add edges only between nodes of opposite node sets
+        G.add_weighted_edges_from(
+            [('a1', 'p2', 1), ('a1', 'p3', 2), ('a2', 'p2', 2), ('a3', 'p1', 1), ('a3', 'p2', 2), ('a2', 'p3', 1)])
+        # our function
+        M = rmm.rank_maximal_matching(G, rank="weight")
+        self.assertEqual(M, matching)
+
+    def test_rank_maximal_matching_unordered_ranks(self):
+        matching = {'a1': 'p2', 'a3': 'p1', 'p2': 'a1','p1': 'a3'}
+        G = nx.Graph()
+        # Add nodes with the node attribute 'bipartite'
+        G.add_nodes_from(['a1', 'a2', 'a3'], bipartite=0)
+        G.add_nodes_from(['p1', 'p2', 'p3'], bipartite=1)
+        # Add edges only between nodes of opposite node sets
+        G.add_weighted_edges_from([('a1', 'p2', 1), ('a1', 'p3', 3), ('a2', 'p2', 4), ('a3', 'p1', 2), ('a3', 'p2', 3)])
+        # our function
+        M = rmm.rank_maximal_matching(G, rank="weight")
+        self.assertEqual(M, matching)
+
+
+
     def check_match(self):
         # example1
         matching = {'a1': 'p2', 'a3': 'p4', 'a2': 'p1','p2': 'a1', 'p4': 'a3','p1':'a2'}
         G = nx.Graph()
         # Add nodes with the node attribute 'bipartite'
-        G.add_nodes_from(['a1', 'a2', 'a3', 'a4'], bipartite=0)
+        G.add_nodes_from(['a1', 'a2', 'a3'], bipartite=0)
         G.add_nodes_from(['p1', 'p2', 'p3', 'p4'], bipartite=1)
         # Add edges only between nodes of opposite node sets
         G.add_weighted_edges_from([('a1', 'p1', 1), ('a1', 'p2', 1), ('a2', 'p2', 1), ('a2', 'p3', 2), ('a3', 'p4', 1)])
         # our function
-        M = rmm.rank_maximal_matching(G) 
+        M = rmm.rank_maximal_matching(G)
 
         self.assertEqual(M, matching)
 
         # example2
+        G.clear()
         matching = {'a1': 'p2', 'p2': 'a1'}
         G.add_nodes_from(['a1', 'a2'], bipartite=0)
         G.add_nodes_from(['p1', 'p2'], bipartite=1)
@@ -28,12 +111,26 @@ class rank_maximal_matching(unittest.TestCase):
         self.assertEqual(M, matching)
 
         # example3
+        G.clear()
         matching = {'a1': 'p1', 'a2': 'p2', 'p1': 'a1', 'p2':'a2'}
-        G.add_nodes_from(['a1', 'a2'], bipartite=0)
+        G.add_nodes_from(['a1', 'a2','a3'], bipartite=0)
         G.add_nodes_from(['p1', 'p2'], bipartite=1)
         G.add_weighted_edges_from([('a1', 'p1', 1), ('a1', 'p2', 2), ('a2', 'p2', 1), ('a3', 'p2', 1)])
         # our function
         M = rmm.rank_maximal_matching(G)
+        self.assertEqual(M, matching)
+
+        G = nx.Graph()
+        G.add_nodes_from(['a1', 'a2', 'a3', 'a4'], bipartite=0)
+        G.add_nodes_from(['p1', 'p2', 'p3', 'p4', 'p5'], bipartite=1)
+        G.add_weighted_edges_from([('a1', 'p1', 1), ('a1', 'p2', 1), ('a1', 'p5', 2),
+                                   ('a2', 'p1', 1), ('a2', 'p2', 2), ('a2', 'p3', 2),
+                                   ('a3', 'p2', 1), ('a3', 'p4', 2), ('a3', 'p5', 1),
+                                   ('a4', 'p3', 2), ('a4', 'p4', 3), ('a4', 'p5', 2)])
+        matching = {'a1': 'p2', 'a2': 'p1', 'a3': 'p5', 'a4': 'p3',
+                    'p2': 'a1', 'p1': 'a2', 'p5': 'a3', 'p3': 'a4'}
+
+        M = rmm.rank_maximal_matching(G, rank="weight")
         self.assertEqual(M, matching)
 
 
